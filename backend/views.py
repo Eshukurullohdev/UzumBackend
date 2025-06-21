129bdbc11e31fc6ce22d5616767f79c834e3dafd
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-06-21 17:03:28 +0500

    some

--- backend/views.py
+++ backend/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, "home.html")
 
-def index(request):
-    return render(request, "index.html")
+def navigation(request):
+    return render(request, "navigation.html")
